8d9bbab755712edf36b6c74928c27a2666b46ac1
Paulo W. Limaverde <vorfast@gmail.com>
2019-03-30 15:34:38 -0300

    0.2 crud pessoas implantado

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import home
+from .views import (
+    home,
+    lista_pessoas,
+    pessoa_novo,
+)
 
 
 urlpatterns = [
-    path('', home, name='core_home'),
+    path('', home,
+         name='core_home'),
+    path('pessoas/', lista_pessoas,
+         name='lista_pessoas'),
+    path('pessoas-novo/', pessoa_novo,
+         name='pessoa_novo'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Pessoa
+from .form import PessoaForm
 
 def home(request):
     return render(request, 'core/index.html')
+
+
+def lista_pessoas(request):
+    data = {}
+    form = PessoaForm()
+    data['pessoas'] = Pessoa.objects.all()
+    data['form'] = form
+    return render(request, 'core/lista_pessoas.html', data)
+
+
+def pessoa_novo(request):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_pessoas')
